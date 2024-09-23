fd9c338331c1345c82be130f6014e27a28fd0fb4
erdey2 <erdeysyoum@gmail.com>
2024-09-23 20:30:31 +0300

    wsgi.py updated

--- room_slot/room_slot/wsgi.py
+++ room_slot/room_slot/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'room_slot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'room_slot.room_slot.settings')
 
 application = get_wsgi_application()
+
+
