5d4e2b0dbece647a621a075ccccb8118cc993018
erdey2 <erdeysyoum@gmail.com>
2024-09-24 12:30:11 +0300

    wsgi.py updated

--- room_slot/room_slot/wsgi.py
+++ room_slot/room_slot/wsgi.py
@@ -8,10 +8,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add the project directory to the sys.path
+sys.path.append('/home/erdey/Desktop/OHRS2/room_slot')  # Change this to the actual path of your OHRS2 directory
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'room_slot.room_slot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'room_slot.settings')
 
 application = get_wsgi_application()
 
