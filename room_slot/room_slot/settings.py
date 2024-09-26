e19f4a4c49ac71b9e9d587565b4b3c18ac912ab9
erdey2 <erdeysyoum@gmail.com>
2024-09-26 12:44:11 +0300

    database changed to mysql

--- room_slot/room_slot/settings.py
+++ room_slot/room_slot/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'room_slot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hotel_db',
+        'USER': 'erdey',
+        'PASSWORD': 'Test@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
