f27fdba2a9b09d8003f63274fd341353e87c7d70
erdey2 <erdeysyoum@gmail.com>
2024-09-28 13:28:17 +0300

    set timezone to UTC

--- room_slot/room_slot/settings.py
+++ room_slot/room_slot/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
         'PASSWORD': 'erdey@123',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'OPTIONS': {
+            'options': '-c timezone=UTC',  # set timezone to UTC
+        },
     }
 }
 
